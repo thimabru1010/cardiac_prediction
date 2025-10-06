1d9b18313f7faa745c6cb59b4cc5d5960fae8dae
Thiago Matheus <thiago.matheus.bruno@gmail.com>
2025-10-06 17:24:05 -0300

    Fixing predictions maps

--- training/base_experiment.py
+++ training/base_experiment.py
@@ -83,7 +83,7 @@ class BaseExperiment:
             inputs, targets = self._unpack_batch(batch)
             with torch.cuda.amp.autocast(enabled=self.mixed_precision):
                 y_region, y_lesion = self.model(inputs)
-                y_pred = combine_lesion_region_preds(y_region, y_lesion, inputs[1])
+                y_pred = combine_lesion_region_preds(y_region, y_lesion, inputs[:, 1])
                 loss = self.criterion(y_pred, targets)
             self.optimizer.zero_grad(set_to_none=True)
             if self.mixed_precision:
