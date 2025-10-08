3831480768076bf2dc32194e1e0e85c75cdeec57
Thiago Matheus <thiago.matheus.bruno@gmail.com>
2025-10-08 16:09:45 -0300

    Debugging training

--- training/base_experiment.py
+++ training/base_experiment.py
@@ -174,10 +174,11 @@ class BaseExperiment:
                 print(
                     f"{'(improved)' if improved else ''}[{epoch}/{epochs}] lr={epoch_stats['lr']:.6f} | "
                     f"train_loss={epoch_stats['train_loss']:.4f} | val_loss={epoch_stats['val_loss']:.4f}\n"
+                    f"train_acc={epoch_stats.get('train_accuracy', float('nan')):.4f} | val_acc={epoch_stats.get('val_accuracy', float('nan')):.4f}\n"
                     f"val_f1_score={epoch_stats.get('val_f1_score', float('nan')):.4f} | "
-                    f"val_precision={epoch_stats.get('val_precision', float('nan')):.4f} | "
+                    f"val_prec={epoch_stats.get('val_prec', float('nan')):.4f} | "
                     f"val_recall={epoch_stats.get('val_recall', float('nan')):.4f} | "
-                    f"val_mIoU={epoch_stats.get('val_mIoU', float('nan')):.4f}\n "
+                    f"val_mIoU={epoch_stats.get('val_mIoU', float('nan')):.4f}\n"
                 )
                 if self.early_stopping.should_stop:
                     print("Early stopping triggered.")
@@ -232,7 +233,7 @@ class BaseExperiment:
 
     def _save_history_json(self):
         hist_path = os.path.join(self.checkpoint_dir, "history.json")
-        print(self.history)
+        # print(self.history)
         with open(hist_path, "w", encoding="utf-8") as f:
             json.dump(self.history, f, indent=2)
 
