09a52aad7de9e3425725b18fa7a42ed3654ce3e0
Thiago Matheus <thiago.matheus.bruno@gmail.com>
2025-11-06 18:23:20 -0300

    Sigmoid used in binary lesion

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
         # bin_les_criterion = nn.CrossEntropyLoss()
         bin_les_criterion = nn.BCEWithLogitsLoss()
     elif args.loss == "focal":
-        class_weights = torch.tensor([1.0, 1.0, 1.0]).to(device)
+        class_weights = torch.tensor([1.0, 1.0, 1.0, 1.0]).to(device)
         class_weights_bin = torch.tensor([1-args.focal_alpha, args.focal_alpha]).to(device)
         multi_les_criterion = FocalLoss(mode="multiclass", gamma=args.focal_gamma, alpha=class_weights)
         bin_les_criterion = FocalLoss(mode="multiclass", gamma=args.focal_gamma, alpha=class_weights_bin)
