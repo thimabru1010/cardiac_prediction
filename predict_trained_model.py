fbe2437b89712535755b9f2e92e957d14cb7feec
Thiago Matheus <thiago.matheus.bruno@gmail.com>
2025-11-17 18:10:17 -0300

    Inference script done for new trained model

--- predict_trained_model.py
+++ predict_trained_model.py
@@ -71,9 +71,9 @@ if __name__ == "__main__":
         multi_lesion_pred = region_preds * binary_preds
         # Save predictions with sitk
         basename = filename.split('.')[0]
-        output_region_path = os.path.join(args.output_dir, f'{basename}_region_pred.nii.gz')
-        output_binary_path = os.path.join(args.output_dir, f'{basename}_binary_pred.nii.gz')
-        output_multi_lesion_path = os.path.join(args.output_dir, f'{basename}_multi_lesion_pred.nii.gz')
+        output_region_path = os.path.join(args.output_dir, f'{basename}_region_lesion_trained_model.nii.gz')
+        output_binary_path = os.path.join(args.output_dir, f'{basename}_binary_lesion_trained_model.nii.gz')
+        output_multi_lesion_path = os.path.join(args.output_dir, f'{basename}_multi_lesion_trained_model.nii.gz')
         
         sitk_region = sitk.GetImageFromArray(region_preds.astype(np.uint8))
         sitk_binary = sitk.GetImageFromArray(binary_preds.astype(np.uint8))
